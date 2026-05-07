8a50413cce776c826b4c85ec949f21fdccceab02
Riley Baker <bbrk24@gmail.com>
2026-05-06 23:33:24 -0400

    Implement text styling on Android

--- Sources/AndroidBackend/Kotlin/AndroidBackendHelpers.kt
+++ Sources/AndroidBackend/Kotlin/AndroidBackendHelpers.kt
@@ -1,7 +1,10 @@
 package dev.swiftcrossui.androidbackend
 
-import android.view.WindowInsets
+import android.R
 import android.app.Activity
+import android.text.TextUtils
+import android.view.WindowInsets
+import android.widget.TextView
 
 class AndroidBackendHelpers {
     fun getWindowWidth(activity: Activity): Int {
@@ -30,5 +33,44 @@ class AndroidBackendHelpers {
         val insets = windowMetrics.getWindowInsets()
                 .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
         return insets.top
+    }
+
+    private var largeTextSize: Float? = null
+    private var titleTextSize: Float? = null
+    private var mediumTextSize: Float? = null
+    private var smallTextSize: Float? = null
+
+    private fun getFontSizeFromResource(activity: Activity, resId: Int) =
+        TextView(activity, null, 0, resId).paint.textSize
+
+    fun clearTextSizeCache() {
+        largeTextSize = null
+        titleTextSize = null
+        mediumTextSize = null
+        smallTextSize = null
+    }
+
+    fun getLargeTextSize(activity: Activity): Float {
+        val size = largeTextSize ?: getFontSizeFromResource(activity, R.style.TextAppearance_DeviceDefault_Large)
+        largeTextSize = size
+        return size
+    }
+
+    fun getTitleTextSize(activity: Activity): Float {
+        val size = titleTextSize ?: getFontSizeFromResource(activity, R.style.TextAppearance_DeviceDefault_WindowTitle)
+        titleTextSize = size
+        return size
+    }
+
+    fun getMediumTextSize(activity: Activity): Float {
+        val size = mediumTextSize ?: getFontSizeFromResource(activity, R.style.TextAppearance_DeviceDefault_Medium)
+        mediumTextSize = size
+        return size
+    }
+
+    fun getSmallTextSize(activity: Activity): Float {
+        val size = smallTextSize ?: getFontSizeFromResource(activity, R.style.TextAppearance_DeviceDefault_Small)
+        smallTextSize = size
+        return size
     }
 }
